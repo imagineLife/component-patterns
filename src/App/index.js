dc5cf228d6dec0f52c7a5c7ca423f28415e657ca
Eric Laursen <ericjlaursen@gmail.com>
2020-06-28 19:21:48 -0400

    Button Props

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Title from '../components/Title'
-import Button from './../components/Button'
+import Button from './../components/ButtonOne'
+// import Button from './../components/Button'
+// import Button from './../components/CompoundButton'
 
-const AppRouter = () => {
+const App = () => {
 	return (<section id="button-props-wrapper">
-			<Title text="Button with props" />
-		
+			<Title text="Buttons" />
 			<Button 
-				text="Plain"
-				large
-				small
-			/>
-			
-			<Button 
-				text="water" 
-				warning
+				// tp="warning" 
+				// size="large" 
+				text="btnOne"
 			/>
 		</section>
 		)
 };
 
-export default AppRouter;
+export default App;
+
+
+/*
+
+	Prop-Driven 
+
+	<Button 
+		text="Plain"
+		large
+		small
+	/>
+
+	<Button 
+		text="water" 
+		warning
+	/>
+
+*/ 
+
+/*
+	Compound
+
+	<Button.Small.Warning text="smWrn"/>		
+	<Button.Danger.Large text="myLg"/>
+*/

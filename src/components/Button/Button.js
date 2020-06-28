4b8934776d61919065a5eccf0151b01da9d6bb18
Eric Laursen <ericjlaursen@gmail.com>
2020-06-28 11:20:09 -0400

    handles props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import './Button.scss';
 
-const Button = ({text, onClick, large, small, secondary, warning}) => {
+const Button = ({text, onClick, large, small, secondary, danger, warning}) => {
 	let className = 'button'
 	let txt = 'Click'
 	
 	/*
 		SIZES
 	*/
+
+	if(large && small){
+		console.warn(`COMPONENT ERROR: don't use multiple sizes in the button`)
+	}
+
 	//medium 
 	if(!large && !small){
 		className = className + ' medium'
 	} 
 
+	if(small){
+		className = className + ' small'
+	}	
+
+
+	if(large){
+		className = className + ' large'
+	}	
+
 
 	/*COLORS*/
 	//primary
-	if(!secondary && !warning){
+	if(!danger && !warning){
 		className = className + ' primary'
+	}	
+
+	if(danger && warning){
+		console.warn(`COMPONENT ERROR: don't use multiple 'types' in the button`)
+	}
+
+	if(warning){
+		className = className + ' warning'
+	}	
+
+	if(danger){
+		className = className + ' danger'
 	}	
 
 	//text
